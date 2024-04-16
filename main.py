035e409860083b0ca1345a55def438fc59bee109
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-04-16 21:18:48 +0000

    A

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+os.system ("pip install -U scratchattach")
+
+import scratchattach as scratch3
+
+session = scratch3.login("Boss_1s", "han2nppQJi^w.p")
+conn = session.connect_cloud("992921739")
+
+events = scratch3.CloudEvents("992921739")
+
+@events.event
+def on_set(event):
+    conn.set_var("FROM_HOST_1")
