1c4abe77b0f802c639e38f49af0c970c00ef5cdf
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-08-10 14:08:10 +0000

    password change

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ os.system ("pip install -U scratchattach")
 
 import scratchattach as scratch3
 
-session = scratch3.login("Boss_1s", "han2nppQJi^w.p")
+session = scratch3.login("Boss_1s", "han2nppQJi^w.p:")
 conn = session.connect_cloud("992921739")
 
 events = scratch3.CloudEvents("992921739")
