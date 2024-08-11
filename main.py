4adc6262657286e58a707943d4efb5f6ffe3f1fe
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-08-11 21:52:19 +0000

    a

--- main.py
+++ main.py
@@ -6,8 +6,4 @@ import scratchattach as scratch3
 session = scratch3.login("Boss_1s", "han2nppQJi^w.p:")
 conn = session.connect_cloud("992921739")
 
-events = scratch3.CloudEvents("992921739")
-
-@events.event
-def on_set(event):
-    conn.set_var("FROM_HOST_1")
+conn.set_var("FROM_HOST_1", 123456789)
