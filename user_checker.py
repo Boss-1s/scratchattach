452cb6958fb83f9c362aae20ff9ea62257ec0058
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-08-15 20:07:27 +0000

    add user checker file

---
+++ user_checker.py
@@ -0,0 +1,27 @@
+"""Module importing scratchattach to use for the projrct"""
+import scratchattach as scratch3
+
+session = scratch3.login("Boss_1sALT", "han2nppQJi^w.p:")
+conn = session.connect_cloud("1054907254") #replace with your project id
+
+client = scratch3.CloudRequests(conn)
+
+@client.request(thread=True)
+def message_ping(argument1):
+    "Main client request"
+    print(f"User existance requested for {argument1}")
+    user = scratch3.get_user(argument1)
+    return user
+
+@client.event
+def on_ready():
+    "Runs when client is ready."
+    print("Request handler is running")
+
+@client.event
+def on_request(request):
+    "Runs when request is recieved."
+    print("Received request", request.name, "Requester:", request.requester, "Request arguments:",
+          request.arguments, "Timestamp:", request.timestamp, "Request ID:", request.id)
+
+client.run() #make sure this is ALWAYS at the bottom of your Python file
