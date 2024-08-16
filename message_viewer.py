d11d56a098efdd2eb1a4f6fcff59dcb3dcfb7c30
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-08-16 14:52:32 +0000

    fix syntax errors

--- message_viewer.py
+++ message_viewer.py
@@ -1,6 +1,6 @@
 """Module importing scratchattach to use for the projrct"""
-import scratchattach as scratch3
 import os
+import scratchattach as scratch3
 
 session = scratch3.login("Boss_1sALT", "han2nppQJi^w.p:")
 conn = session.connect_cloud("1054907254") #replace with your project id
@@ -26,7 +26,7 @@ def on_request(request):
     "Runs when request is recieved."
     print("Received request", request.name, "Requester:", request.requester, "Request arguments:",
           request.arguments, "Timestamp:", request.timestamp, "Request ID:", request.id)
-    os.system(f"echo Received request {request.name}, Requester: {request.requester}, Request arguments:
-          {request.arguments}, Timestamp: {request.timestamp}, Request ID: {request.id}")
+    os.system(f"echo Received request {request.name}, Requester: {request.requester}, Request arguments:",
+          f"{request.arguments}, Timestamp: {request.timestamp}, Request ID: {request.id}")
 
 client.run() #make sure this is ALWAYS at the bottom of your Python file
