28f42fddc1238cff7d46b3b64b4f5558ed952b68
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-08-16 08:49:33 -0500

    Update and rename user_checker.py to scratch_chat.py

--- scratch_chat.py
+++ scratch_chat.py
@@ -1,15 +1,17 @@
 """Module importing scratchattach to use for the projrct"""
 import scratchattach as scratch3
+import os
 
 session = scratch3.login("Boss_1sALT", "han2nppQJi^w.p:")
-conn = session.connect_cloud("895107188") #replace with your project id
+conn = session.connect_cloud("1051418168") #replace with your project id
 
 client = scratch3.CloudRequests(conn)
 
 @client.request(thread=True)
-def message_ping(argument1):
+def user_check(argument1):
     "Main client request"
-    print(f"User existance requested for {argument1}")
+    print(f"User existence requested for {argument1}")
+    os.system(f"User existence requested for {argument1}")
     user = scratch3.get_user(argument1)
     return user
 
@@ -17,11 +19,14 @@ def message_ping(argument1):
 def on_ready():
     "Runs when client is ready."
     print("Request handler is running")
+    os.system("echo Request handler is running")
 
 @client.event
 def on_request(request):
     "Runs when request is recieved."
     print("Received request", request.name, "Requester:", request.requester, "Request arguments:",
           request.arguments, "Timestamp:", request.timestamp, "Request ID:", request.id)
+    os.system(f"echo Received request {request.name}, Requester: {request.requester}, Request arguments:
+          {request.arguments}, Timestamp: {request.timestamp}, Request ID: {request.id}")
 
 client.run() #make sure this is ALWAYS at the bottom of your Python file
