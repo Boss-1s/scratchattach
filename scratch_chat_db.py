6f1d15238f3d44c0284100cae74aca59537189f6
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2025-07-08 11:36:07 -0500

    Update scratch_chat_db.py

--- scratch_chat_db.py
+++ scratch_chat_db.py
@@ -10,7 +10,7 @@ session = scratch3.login("Boss_1sALT", passwrd)
 cloud = session.connect_cloud("1051418168") #<- v2.0 dev project id, not the real one!
 
 #db
-storage = cloud.storage()
+storage = cloud.storage(used_cloud_vars=["3","4"])
 
 db1 = db("chat", json_file_path="chat_db.json", save_interval=5)
 db2 = db("history", json_file_path="chat_history_db.json", save_interval=5)
