863bdf9ae291f16cd3e9858ebb5648ecca46d818
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2025-07-10 10:58:05 -0500

    Update scratch_chat_db.py

--- scratch_chat_db.py
+++ scratch_chat_db.py
@@ -12,8 +12,8 @@ cloud = session.connect_cloud("1051418168") #<- v2.0 dev project id, not the rea
 #db
 storage = cloud.storage(used_cloud_vars=["3","4"])
 
-db1 = db("chat", json_file_path="chat_db.json", save_interval=5)
-db2 = db("history", json_file_path="chat_history_db.json", save_interval=5)
+db1 = db("chat", json_file_path="chat_db.json", save_interval=60)
+db2 = db("history", json_file_path="chat_history_db.json", save_interval=60)
 
 storage.add_database(db1)
 storage.add_database(db2)
