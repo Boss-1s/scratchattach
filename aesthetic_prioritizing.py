61f6b7e0f63b90bc9e0bd71101fbb77e28752590
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2025-08-06 10:50:58 -0500

    Update aesthetic_prioritizing.py

--- aesthetic_prioritizing.py
+++ aesthetic_prioritizing.py
@@ -31,6 +31,14 @@ def favorites():
     for project in favorited_projects:
         fav_project_ids.append(project.id)
 
+def color_text(str, textColor, defaultColor = "\033[0m"):
+    if textColor[0] != "\\" or defaultColor[0] != "\\":
+        raise TypeError("clor must be an ASNI escape code.")
+        
+    print(f"{textColor}{str}{defaultColor}")
+        
+    
+
 
 max_atmp = 5
 atmp = 0
@@ -45,8 +53,8 @@ def prioritize(attempt: int, maxAttempts: int):
         if attempt >= maxAttempts:
             raise ValueError("Process failed, and the maximum attempt value has been reached. Exiting.")
         #debug
-        print(project_ids_to_prioritize)
-        print(fav_project_ids[:len(project_ids_to_prioritize)])
+        color_text("\033[35m",project_ids_to_prioritize)
+        color_text("\033[35m",fav_project_ids[:len(project_ids_to_prioritize)])
         if project_ids_to_prioritize == fav_project_ids[:len(project_ids_to_prioritize)]:
             print("Projects already on top. No prioritizing needed.")
             return
