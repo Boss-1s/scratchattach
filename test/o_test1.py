a1d83958ce82cd3e085bc60f05ba015479948590
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2025-12-31 10:17:40 -0600

    refactor: shrink down test file + add assertion

--- test/o_test1.py
+++ test/o_test1.py
@@ -1,16 +1,10 @@
-from typing import get_type_hints
-import time
-import sys
-import os
-import json
+import time,sys,os,json
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 from key_multivalue_storage import Storage as s
 
-sload = s.Load
-sdelete = s.Delete
-sedit = s.Edit
+file = "otest.json"
 
 #@client.request(response_priority=2)
 def db_add_subval(db, key, subkey, val, subkey2=None, val2=None, subkey3=None, val3=None, subkey4=None, val4=None, subkey5=None, val5=None):
@@ -72,19 +66,22 @@ def db_delete_all(db):
     print(f"Sucessfully deleted all data from {db}.")
     #return "Success"
 
-db_add_subval("otest.json", "test_top_level_key", "sk1", "val1", "sk2", "skibidi toilet")
-assert db_get_keys("otest.json") == "test_top_level_key", "failed at debug point 1"
-db_add_subval("otest.json", "test_top_level_key2", "sk1", "val1", "sk2", "the fate of sigma")
-db_add_subval("otest.json", "test_top_level_key2", "sk3", "val3", "sk4", "the fate of shard")
-time.sleep(2)
-db_set_val("otest.json", "test_top_level_key", "sk2", "sigma")
-time.sleep(2)
-print(db_get_keys("otest.json"))
-time.sleep(1)
-print(db_get_subkeys_values("otest.json", "test_top_level_key"))
-time.sleep(1)
-db_delete_key("otest.json", "test_top_level_key2")
-time.sleep(1)
-db_delete_all("otest.json")
-
+db_add_subval(file, "test_top_level_key", "sk1", "val1", "sk2", "skibidi toilet")
+assert db_get_keys(file) == ["test_top_level_key"], "Adding a top level key has failed. Try checking the store method."
+assert db_get_subkeys_values(file, "test_top_level_key") == ["sk1: val1","sk2: skibidi toilet"],"Adding subkey-value pairs has failed. Try checking the store method."
+assert db_get_subkeys_values(file, "test_top_level_key", keys=False) == ["val1","skibidi toilet"],"Adding subkey-value pairs has failed. Try checking both the store and db_add_subval methods."
+db_add_subval(file, "test_top_level_key2", "sk1", "val1", "sk2", "the fate of sigma")
+assert db_get_keys(file) == ["test_top_level_key","test_top_level_key2"], "Adding a top level key has failed. Try checking the store method."
+assert db_get_subkeys_values(file, "test_top_level_key2") == ["sk1: val1","sk2: the fate of sigma"],"Adding subkey-value pairs has failed. Try checking the store method."
+assert db_get_subkeys_values(file, "test_top_level_key2", keys=False) == ["val1","the fate of sigma"],"Adding subkey-value pairs has failed. Try checking both the store and db_add_subval methods."
+db_add_subval(file, "test_top_level_key2", "sk3", "val3", "sk4", "the fate of shard")
+assert db_get_keys(file) == ["test_top_level_key", "test_top_level_key2"], "Adding a top level key has failed. Try checking the store method."
+assert db_get_subkeys_values(file, "test_top_level_key2") == ["sk1: val1","sk2: the fate of sigma","sk3: val3","sk4: the fate of shard"],"Adding subkey-value pairs has failed. Try checking the store method."
+assert db_get_subkeys_values(file, "test_top_level_key2", keys=False) == ["val1","the fate of sigma","val3","the fate of shard"],"Adding subkey-value pairs has failed. Try checking both the store and db_add_subval methods."
+db_set_val(file, "test_top_level_key", "sk2", "sigma")
+assert db_get_subkeys_values(file, "test_top_level_key") == ["sk1: val1","sk2: sigma",],"Editing subkey-value pairs has failed. Try checking the Edit.propval method."
+db_delete_key(file, "test_top_level_key2")
+assert db_get_keys(file) == ["test_top_level_key"], "Deleting a key has failed. Check the Delete.by_key method."
+db_delete_all(file)
+assert db_get_keys(file) == [], "Bulk deleting has failed. Check the Delete.all method."
 print("\ntest passed sucessfully")
